82cf98f43a40b76551fca039ce14f1524d8a6411
Diego Martinez <718717@student.dusd.net>
2024-05-31 07:42:34 -0700

    I fixed the enemy movment script
    the enemy movment script was being buggy so i fixed it

--- Final/Assets/Scripts/Enemy script.cs
+++ Final/Assets/Scripts/Enemy script.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Charactermovment : MonoBehaviour
+public class Enemyscript : MonoBehaviour
 {
-    public GameObject Player2;
     public GameObject Unit;
+    public GameObject Enemy;
     public float speed;
     // Start is called befre the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Unit.transform.position = Vector3.MoveTowards(Unit.transform.position, Player2.transform.position, speed);
+        Enemy.transform.position = Vector3.MoveTowards(Enemy.transform.position, Unit.transform.position, speed);
     }
 }
