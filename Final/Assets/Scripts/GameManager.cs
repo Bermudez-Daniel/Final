3c1060f984fb40a0ce5be428a62d457802aa04b2
Diego Martinez <718717@student.dusd.net>
2024-05-31 10:45:11 -0700

    I added pause button
    I added a pause button and a on collision detection script

--- Final/Assets/Scripts/GameManager.cs
+++ Final/Assets/Scripts/GameManager.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject[] UnitPrefabs;
-    private float spawnRangeX = 20;
+    private float spawnRangeX = 51;
     private float spawnPosZ = 0;
     private float startDelay = 2;
     private float spawnInterval = 1.5f;
+    public TextMeshProUGUI gameOverText;
+    public GameObject pauseScreen;
+    private bool paused;
 
     private void Start()
     {
         InvokeRepeating("SpawnRandomUnit", startDelay, spawnInterval);
+        gameOverText.gameObject.SetActive(true);
     }
 
     void Update()
     {
-       
+       if (Input.GetKeyDown(KeyCode.P))
+        {
+            ChangePaused();
+        }
     }
 
     void SpawnRandomUnit ()
@@ -26,5 +34,26 @@ public class GameManager : MonoBehaviour
         int UnitIndex = Random.Range(0, UnitPrefabs.Length);
         Instantiate(UnitPrefabs[UnitIndex], spawnPos,
             UnitPrefabs[UnitIndex].transform.rotation);
+    }
+
+    public void StartGame()
+    {
+       
+    }
+
+    void ChangePaused()
+    {
+        if (!paused)
+        {
+            paused = true;
+            pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            paused = false;
+            pauseScreen.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
